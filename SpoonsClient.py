473df5e7501c305aa7c09372b49709369ca1a61c
Meghan Novak <mnovak5@nd.edu>
2023-04-26 16:54:28 -0400

    Spoon grab first implementation

--- SpoonsClient.py
+++ SpoonsClient.py
@@ -3,6 +3,9 @@ import socket
 import json
 import http.client
 import time
+import struct 
+import select
+
 
 class SpoonsClient:
     heart = "\u2665"
@@ -19,12 +22,19 @@ class SpoonsClient:
         self.server_retries = 0
         self.send_retries = 0
         self.recv_retries = 0
+        self.id = -1
+        self.grabbing_started = 0
+        self.eliminated = 0
+        # self.multicast_group = '224.3.29.71'
+        # self.spoon_server_address = ('', 10000)
+        # self.spoon_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         self.mycards = []
         
         # lookup name and connect to server
         self.connect_to_server()
-        self.play_game()
+        while(not self.eliminated):
+            self.play_game()
 
     def find_name(self):
         while(True):
@@ -36,8 +46,6 @@ class SpoonsClient:
                 try:
                     if self.host == None and entry['game_name'] == self.game_name: # save the first match
                         self.host = entry['name']
-                        print('host:', entry['name'])
-                        print('owner:', entry['owner'])
                         self.port = entry['port']
                         self.lastheardfrom = entry['lastheardfrom']
 
@@ -61,6 +69,7 @@ class SpoonsClient:
     def connect_to_server(self):
         # find in name server and connect
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         self.s.settimeout(30)
         self.find_name()
 
@@ -68,11 +77,15 @@ class SpoonsClient:
         while(True):
             try:
                 res = self.s.connect((self.host, self.port))
+                resp = self.recv_resp('')
+                self.id = int(resp['id'])
                 print('Connected to port: ', self.port)
+                print('Welcome! You are player ' + str(self.id) + '!')
+                if self.id == 0:
+                    print('\nYou are the first player in the circle! You will be picking up from the remaining deck and will begin the flow of cards.')
                 break
             except Exception as e:
                 print('Connection to server failed. Restarting connection.')
-                print("tried port: ", self.port)
                 try:
                     self.s.close()
                 except:
@@ -87,70 +100,114 @@ class SpoonsClient:
         # server will send hand of cards
         self.get_cards()
 
-        while(True):
+        # join UDP multicast group for spoon grab messages
+        # self.spoon_sock.bind(self.spoon_server_address)
+        # group = socket.inet_aton(self.multicast_group)
+        # mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        # self.spoon_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+
+        while(self.grabbing_started == 0):
+        
             print('\nYOUR HAND:')
             self.display_cards(self.mycards, 1)
 
-            method = input("Enter 'p' to pickup next card\n")
-            if method != 'p':
+            method = input("Enter 'p' to pickup next card\nEnter 'x' to try to grab a spoon\n")
+
+            if method != 'p' and method != 'x':
                 print('Invalid operation')
                 continue
 
-            new_card = self.pickup()
-            if new_card == None:
-                print('No cards in pick up deck yet. Try again.')
-                continue
-            
-            self.mycards.append(new_card)
-            print('NEW CARD:')
-            self.display_cards([new_card], 1)
+            if method == 'x':
+                print('GRABBING SPOON')
+                self.grab_spoon()
 
-            for i, card in enumerate(self.mycards):
-                print(str(i) + ': ', end='')
-                self.display_cards([card], 0)
-            ind = int(input("\tEnter card to discard (0-4): "))
+            if method == 'p':
+                new_card = self.pickup()
+                if new_card == 'next_round':
+                    return
+                elif new_card == 'eliminated':
+                    return
 
-            while ind < 0 or ind > 4:
-                print('\tInvalid card selected.')
-                ind = int(input("\tEnter card to discard (0-4)"))
+                elif new_card == None:
+                    print('No cards in pick up deck yet. Try again.')
+                    continue
+                
+                self.mycards.append(new_card)
+                print('NEW CARD:')
+                self.display_cards([new_card], 1)
 
-            discard_card = self.mycards[ind]
-            self.discard(discard_card)
-            self.mycards.remove(discard_card)
-            
-            #if self.four_of_a_kind():
-            # instead of checking if four of a kind, have the option to alaways grab spoon, the func will check if four of a kind
-            self.grab_spoon()
+                for i, card in enumerate(self.mycards):
+                    print(str(i) + ': ', end='')
+                    self.display_cards([card], 0)
 
-            # if resp is 'eliminated':
-            #   break
+                ind = input("\tEnter card to discard (0-4): ")
+
+                while ind not in ['0', '1', '2', '3', '4']:
+                    print('\tInvalid card selected.')
+                    ind = input("\tEnter card to discard (0-4)")
+
+                ind = int(ind)
+
+                discard_card = self.mycards[ind]
+                resp = self.discard(discard_card)
+                if resp == 'next_round' or resp == 'eliminated':
+                    return
+
+                self.mycards.remove(discard_card)
+
+    # def check_spoon_grab(self):
+    #     #while 1:
+    #     print('checking spoons')
+    #     ready, _, _ = select.select([self.spoon_sock], [], [], 0)
+    #     for s in ready:
+    #         print('got in here')
+    #         data, addr = self.spoon_sock.recvfrom(1024)
+    #         data = json.loads(data.decode())
+    #         if data['msg'] == 'SPOON GRABBED! YOU HAVE 10 SECONDS TO TRY AND GRAB A SPOON!':
+                
+    #             print(data['msg'], flush=True)
+    #             method = input('ENTER x TO TRY TO GRAB A SPOON!\n')
+    #             self.spoon_sock.sendto('ack', addr)
 
     def grab_spoon(self):
-        print("Are you ready to grab a spoon?")
-        wantSpoon = input("PRESS X TO GRAB SPOON!")
+        # print("Are you ready to grab a spoon?")
+        # wantSpoon = input("PRESS x TO GRAB SPOON!")
         # user gets correct card line up and grabs spoon
-        if wantSpoon == 'X\n':
-            if self.four_of_a_kind():
-                currTime = time.time_ns()
-                # check if im doing the messages right, just doing it based off of the code I saw meghan right
-                msg = { 'method': 'grab_spoon', 'time': str(currTime)}
-                msg = json.dumps(msg)
-                self.send_request(msg)
-                resp = self.recv_resp(msg)
-                server_ack = json.loads(msg)
-                status = server_ack['status']
-                if status == 'success':
-                    if server_ack['spoons_left'] == 0:
-                        print("You got the last spoon. You win!!")
-                    else:
-                        print("You successfully grabbed a spoon!\nWait for the other players to grab the spoons for the next round.")
-                 ## print whether eliminated or moving on
-                 ##return resp
-            else:
-                print("Invalid cards to grab spoon. Keep playing!")
-       
-       
+        # if wantSpoon == 'x\n':
 
+
+        if self.four_of_a_kind() or self.grabbing_started:
+            msg = { 'method': 'grab_spoon'}
+            msg = json.dumps(msg)
+            self.send_request(msg)
+            resp = self.recv_resp(msg)
+            if resp['result'] == 'next_round':
+                print('SUCCESS!')
+                print('\tYou successfully grabbed a spoon. Moving on to the next round.')
+                return 'next_round'
+            elif resp['result'] == 'eliminated':
+                print('TOO SLOW')
+                print('\tYou were last to grab a spoon. You have been ELIMINATED.')
+                self.eliminated = 1
+                return 'eliminated'
+
+            # print('before')
+            # data, addr = self.spoon_sock.recvfrom(1024)
+            # print('after')
+
+            # server_ack = json.loads(msg)
+            # status = server_ack['status']
+            # if status == 'success':
+            #     if server_ack['spoons_left'] == 0:
+            #         print("You got the last spoon. You win!!")
+            #     else:
+            #         print("You successfully grabbed a spoon!\nWait for the other players to grab the spoons for the next round.")
+                ## print whether eliminated or moving on
+                ##return resp
+        else:
+            print("\nInvalid cards to grab spoon. Keep playing!")
+       
 
     def get_cards(self):
         msg = { 'method': 'get_cards' }
@@ -165,6 +222,11 @@ class SpoonsClient:
         msg = json.dumps(msg)
         self.send_request(msg)
         resp = self.recv_resp(msg)
+        if resp['method'] == 'GRAB':
+            self.grabbing_started = 1
+            x = input('GRABBING STARTED!\n\tENTER x TO GRAB! : ')
+            return self.grab_spoon()
+
         if resp['result'] == 'success':
             return resp['card']
         else:
@@ -175,7 +237,11 @@ class SpoonsClient:
         msg = json.dumps(msg)
         self.send_request(msg)
         resp = self.recv_resp(msg)
-        ### ??? necessary to return anything here?
+        if resp['method'] == 'GRAB':
+            self.grabbing_started = 1
+            x = input('GRABBING STARTED!\n\tENTER x TO GRAB!')
+            return self.grab_spoon()
+        return None
   
     def send_request(self, msg):
         self.send_retries = 0
@@ -234,64 +300,66 @@ class SpoonsClient:
         suits = [0,0,0,0]
         tens = [0,0,0,0] # array of booleans saying if its a ten or not
 
-        for index, card in enumerate(cards):
-            if card[-1:] == 'H':
-                suits[index] = self.heart
-            elif card[-1:] == 'C':
-                suits[index] = self.club
-            elif card[-1:] == 'D':
-                suits[index] = self.diamond
-            elif card[-1:] == 'S':
-                suits[index] = self.spade
-            if card[:-1] == 10:
-                tens[index]=1
-            else:
-                tens[index]=0
+        if(len(cards) == 4):
 
-        if graphics:
-            # check that tens works
-            # adjust for different spacing with 10 (two digit number)
-            print(f'\t+-----+\t\t+-----+\t\t+-----+\t\t+-----+\t\t')
-            if tens[0]==1:
-                print(f'\t|{cards[0][:-1]}   |\t', end='')
+            for index, card in enumerate(cards):
+                if card[-1:] == 'H':
+                    suits[index] = self.heart
+                elif card[-1:] == 'C':
+                    suits[index] = self.club
+                elif card[-1:] == 'D':
+                    suits[index] = self.diamond
+                elif card[-1:] == 'S':
+                    suits[index] = self.spade
+                if card[:-1] == '10':
+                    tens[index]=1
+                else:
+                    tens[index]=0
+
+            if graphics:
+                # check that tens works
+                # adjust for different spacing with 10 (two digit number)
+                print(f'\t+-----+\t\t+-----+\t\t+-----+\t\t+-----+\t\t')
+                if tens[0]==1:
+                    print(f'\t|{cards[0][:-1]}   |\t', end='')
+                else:
+                    print(f'\t|{cards[0][:-1]}    |\t', end='')
+                if tens[1]==1:
+                    print(f'\t|{cards[1][:-1]}   |\t', end='')
+                else:
+                    print(f'\t|{cards[1][:-1]}    |\t', end='')
+                if tens[2]==1:
+                    print(f'\t|{cards[2][:-1]}   |\t', end='')
+                else:
+                    print(f'\t|{cards[2][:-1]}    |\t', end='')
+                if tens[3]==1:
+                    print(f'\t|{cards[3][:-1]}   |\t\t')
+                else:
+                    print(f'\t|{cards[3][:-1]}    |\t\t')
+                print(f'\t|{suits[0]}    |\t\t|{suits[1]}    |\t\t|{suits[2]}    |\t\t|{suits[3]}    |\t\t')
+                print(f'\t|     |\t\t|     |\t\t|     |\t\t|     |\t\t')
+                print(f'\t+-----+\t\t+-----+\t\t+-----+\t\t+-----+\t\t')
             else:
-                print(f'\t|{cards[0][:-1]}    |\t', end='')
-            if tens[1]==1:
-                print(f'\t|{cards[1][:-1]}   |\t', end='')
-            else:
-                print(f'\t|{cards[1][:-1]}    |\t', end='')
-            if tens[2]==1:
-                print(f'\t|{cards[2][:-1]}   |\t', end='')
-            else:
-                print(f'\t|{cards[2][:-1]}    |\t', end='')
-            if tens[3]==1:
-                print(f'\t|{cards[3][:-1]}   |\t\t')
-            else:
-                print(f'\t|{cards[3][:-1]}    |\t\t')
-            print(f'\t|{suits[0]}    |\t\t|{suits[1]}    |\t\t|{suits[2]}    |\t\t|{suits[3]}    |\t\t')
-            print(f'\t|     |\t\t|     |\t\t|     |\t\t|     |\t\t')
-            print(f'\t+-----+\t\t+-----+\t\t+-----+\t\t+-----+\t\t')
+                print(f'{card[:-1]}{suit}')
+
+
         else:
-            print(f'{card[:-1]}{suit}')
 
+            for card in cards:
+                if card[-1:] == 'H':
+                    suit = self.heart
+                elif card[-1:] == 'C':
+                    suit = self.club
+                elif card[-1:] == 'D':
+                    suit = self.diamond
+                elif card[-1:] == 'S':
+                    suit = self.spade
 
-
-
-        # for card in cards:
-        #     if card[-1:] == 'H':
-        #         suit = self.heart
-        #     elif card[-1:] == 'C':
-        #         suit = self.club
-        #     elif card[-1:] == 'D':
-        #         suit = self.diamond
-        #     elif card[-1:] == 'S':
-        #         suit = self.spade
-
-        #     if graphics:
-        #         # adjust for different spacing with 10 (two digit number)
-        #         if card[:-1] == '10':
-        #             print(f'\t+-----+\n\t|{card[:-1]}   |\n\t|{suit}    |\n\t|     |\n\t+-----+')
-        #         else:
-        #             print(f'\t+-----+\n\t|{card[:-1]}    |\n\t|{suit}    |\n\t|     |\n\t+-----+')
-        #     else:
-        #         print(f'{card[:-1]}{suit}')
+                if graphics:
+                    # adjust for different spacing with 10 (two digit number)
+                    if card[:-1] == '10':
+                        print(f'\t+-----+\n\t|{card[:-1]}   |\n\t|{suit}    |\n\t|     |\n\t+-----+')
+                    else:
+                        print(f'\t+-----+\n\t|{card[:-1]}    |\n\t|{suit}    |\n\t|     |\n\t+-----+')
+                else:
+                    print(f'{card[:-1]}{suit}')
--- SpoonsServer.py
+++ SpoonsServer.py
@@ -5,7 +5,7 @@ import os
 import select
 from CardDeck import *
 import time
-import asynchio
+import asyncio
 
 class SpoonsServer:
     def __init__(self, game_name, expected_players):
@@ -15,6 +15,11 @@ class SpoonsServer:
         self.game_name = game_name
         self.last_sent = 0
         self.BroadCastQueue = []
+        # self.multicast_group = ('224.3.29.71', 10000)
+        # self.multicast_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self.multicast_sock.settimeout(0.2)
+        # self.multicast_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, (b'1'))
+
         
         self.players = []
         self.players_info = {}
@@ -24,7 +29,8 @@ class SpoonsServer:
         self.deck = CardDeck()
         self.discard_pile = []
         self.timeSpoonGrabbed = -1
-        # ??? self.spoon_port = 
+        self.first_spoon_grabbed = 0
+        self.moving_forward = []
 
         # for port already used error
         for i in range(1,15):
@@ -44,7 +50,6 @@ class SpoonsServer:
 
         # will continue playing games until server is killed
         while True:
-            # ask meghan, is this a timeout for players to join??
             if self.last_sent == 0 or time.time_ns() - self.last_sent > 6e+10:
                 self.send_udp()
                 self.last_sent = time.time_ns()
@@ -60,8 +65,11 @@ class SpoonsServer:
 
                 (new_player, addr) = self.master.accept()
                 self.players.append(new_player)
-                self.init_player_info(new_player)
+                self.init_player_info(new_player, self.num_players)
                 print('\tPlayer', self.num_players, 'joined!')
+                msg = { 'method': 'join_game', 'status': 'success', 'id': self.num_players }
+                resp = json.dumps(msg).encode()
+                new_player.send(resp)
                 self.num_players += 1
                 self.game_init_time = time.time_ns()
 
@@ -69,31 +77,16 @@ class SpoonsServer:
             # if self.num_players == 0:
             #     continue
 
-            print('Starting game!')
-            self.play_game()
+            while self.num_players > 1:
+                print('Starting game!')
+                self.play_game() 
 
-    def spoons_thread(self,player,msg):
-        msg = json.loads(msg)
-        method = msg['method']
-        if self.num_spoons == self.num_players - 1: # need to broadcast to everyone else to get spoon, and that first grabber got it
-                self.timeSpoonGrabbed = int(msg['time'])
-                self.num_spoons -= 1
-                ack_msg = {'method': "grab_spoon", 'status': 'success','spoons_left': str(num_spoons)}
-                response = self.execute_msg(player, ack_msg)
-                response = json.dumps(response)
-                player.send(response.encode())
-                self.players_info[player]['spoon_grabbed'] = 1
-                broadcast_msg = {'method': 'grab_spoon', 'spoons_left': str(num_spoons)}
-                for p in players_info:
-                    if players_info[p]['spoon_grabbed'] == 0
-                        BroadCastQueue.append(players_info[p])
-                        # need to broadcast at once.... 
-            else: # otherwise its a race condition.. might want to lock this section and make it in a thread..
-                
-
+            #### send winner message to the winning player 
 
     def play_game(self):
         self.deal_cards()
+
+        self.first_spoon_grabbed = 0
         self.num_spoons = self.num_players - 1
         # Set pickup pile of player #0 to be remaining_cards in deck object
         self.players_info[self.players[0]]['pickup_deck'] = self.deck.remaining_cards
@@ -115,8 +108,6 @@ class SpoonsServer:
                     self.spoons -= 1
                     continue  # continue to next round
 
-
-            #### ??? right now i have this using chunking like if client sends length of msg, not sure if we wanted this so we can def take out
             try: 
                 bytes_to_read = player.recv(2).decode()
             except ConnectionResetError as cre:
@@ -145,14 +136,24 @@ class SpoonsServer:
                 msg += player.recv(int(bytes_to_read) - bytes_read).decode()
 
             response = self.execute_msg(player, msg)
+
+            # eliminate player
+            if response['result'] == 'eliminated':
+                response = json.dumps(response)
+                player.send(response.encode())
+                self.players_info.pop(player)
+                self.players.remove(player)
+                return
+
             response = json.dumps(response)
             player.send(response.encode())
             
         return
 
     # adds a player to player_info and intiializes it's values
-    def init_player_info(self, player):
+    def init_player_info(self, player, num):
         self.players_info[player] = {
+                                        'id': num,
                                         'cards': [],
                                         'pickup_deck': [],
                                         'player_num': self.num_players,
@@ -160,33 +161,40 @@ class SpoonsServer:
                                     }
 
     def execute_msg(self, player, msg):
+        grab_msg = { 'method': 'GRAB', 'result': '' }
         msg = json.loads(msg)
         method = msg['method']
 
         # TODO: handle get_cards, pickup, putdown, and spoon requests from clients
         if method == 'get_cards':
-            resp = { 'result': 'success', 'cards': self.players_info[player]['cards'] }
+            resp = { 'method': 'get_cards', 'result': 'success', 'cards': self.players_info[player]['cards'] }
 
         elif method == 'pickup':
+            if self.first_spoon_grabbed:
+                return grab_msg
+
             if len(self.players_info[player]['pickup_deck']) == 0:
 
                 if player == self.players[0]:
                     if self.discard_pile == []:
-                        resp = { 'result': 'failure', 'message': 'No cards in pickup deck. Try again.' }
+                        resp = { 'method': 'pickup', 'result': 'failure', 'message': 'No cards in pickup deck. Try again.' }
                         return resp
                     else:
                         self.players_info[player]['pickup_deck'] = self.discard_pile
                         self.discard_pile = []
 
                 else:
-                    resp = { 'result': 'failure', 'message': 'No cards in pickup deck. Try again.' }
+                    resp = { 'method': 'pickup', 'result': 'failure', 'message': 'No cards in pickup deck. Try again.' }
                     return resp
 
             new_card = self.players_info[player]['pickup_deck'].pop()
             self.players_info[player]['cards'].append(new_card)
-            resp = { 'result': 'success', 'card': new_card }
+            resp = { 'method': 'pickup', 'result': 'success', 'card': new_card }
 
         elif method == 'discard':
+            if self.first_spoon_grabbed:
+                return grab_msg
+
             self.players_info[player]['cards'].remove(msg['card'])
             next_ind = self.players_info[player]['player_num'] + 1
 
@@ -197,13 +205,89 @@ class SpoonsServer:
             else:
                 self.players_info[self.players[next_ind]]['pickup_deck'].append(msg['card'])
 
-            resp = { 'result': 'success' }
-        elif method == 'grab_spoon':
-            self.spoons_thread(player, msg)
-            # ??
-            return
+            resp = { 'method': 'discard', 'result': 'success' }
+
+        elif method == 'grab_spoon':  
+
+            # first spoon to be grabbed
+            if self.first_spoon_grabbed == 0:
+                self.first_spoon_grabbed = 1
+                self.num_spoons -= 1
+                resp = {'result': 'next_round'}
+
+            elif self.num_spoons > 0:
+                self.num_spoons -= 1
+                resp = {'result': 'next_round'}
+
+            # remove last to grab
+            elif self.num_spoons == 0:
+                self.num_players -= 1
+                resp = { 'result': 'eliminated' }
 
         return resp
+
+
+    # def spoon_multicast(self):
+    #     self.multicast_sock.settimeout(5)
+
+    #     # send msg to the multicast group
+    #     print('1 here')
+    #     msg = json.dumps({ 'msg': 'SPOON_GRABBED! YOU HAVE 10 SECONDS TO TRY AND GRAB A SPOON!' }).encode()
+        
+    #     #try:
+    #     #result = self.multicast_sock.sendto(msg, self.multicast_group)
+    #     self.timeSpoonGrabbed = time.time_ns()
+    #     print('2 here')
+    #     # wait for responses from all players for 10s
+    #     while time.time_ns() - self.timeSpoonGrabbed < 10e+9:
+    #         result = self.multicast_sock.sendto(msg, self.multicast_group)
+
+    #         print('3 here', result)
+    #         try: 
+    #             data, p = self.multicast_sock.recvfrom(1024)
+    #             print('4 here')
+    #             self.moving_forward.append(p)
+    #             self.players_info[p].spoon_grabbed = 1
+    #             print('received', data, 'from', p)
+
+    #         except socket.timeout:
+    #             print('too slow')
+
+
+        # # notify eliminated players
+        # for p in self.players:
+        #     if p not in self.moving_forward:
+        #         msg = json.dumps('ELIMINATED').encode()
+        #         p.send(msg)
+        #         self.players_info.pop(p)
+        #         self.players.remove(p)
+        #     else:
+        #         msg = json.dumps('SPOON GRABBED. MOVING TO NEXT ROUND.').encode()
+        #         p.send(msg)
+
+
+        ### ? how to handle failure?
+        # except Exception as e:
+        #     print("FAILED", e)
+
+    def spoons_thread(self, player, msg):
+        if self.num_spoons == self.num_players - 1: # need to broadcast to everyone else to get spoon, and that first grabber got it
+            self.timeSpoonGrabbed = int(msg['time'])
+            self.num_spoons -= 1
+            ack_msg = {'method': "grab_spoon", 'status': 'success','spoons_left': str(num_spoons)}
+            response = json.dumps(response)
+            player.send(response.encode())
+            self.players_info[player]['spoon_grabbed'] = 1
+            broadcast_msg = {'method': 'grab_spoon', 'spoons_left': str(num_spoons)}
+            for p in players_info:
+                if players_info[p]['spoon_grabbed'] == 0:
+                    self.BroadCastQueue.append(players_info[p])
+                    # need to broadcast at once.... 
+        #else: # otherwise its a race condition.. might want to lock this section and make it in a thread..
+            
+
+  
+
 
 
 
