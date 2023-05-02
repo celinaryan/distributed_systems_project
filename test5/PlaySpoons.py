ba19a8c490d76a4069e55c997b75d5c4308496d9
Celina Ryan <cryan27@nd.edu>
2023-05-02 18:34:34 -0400

    new

---
+++ test5/PlaySpoons.py
@@ -0,0 +1,12 @@
+from SpoonsClient import *
+import sys
+if __name__=='__main__':
+    if len(sys.argv) != 2:
+        print('Invalid number of arguments')
+        print('Usage: python3 PlaySpoons.py <GAME_NAME>')
+        exit(1)
+    else:
+        game_name = sys.argv[1]
+
+    player = SpoonsClient(game_name)
+    
