78c70d50ad6ec26fc20140225304d156615d087b
joshhchun <jchun2@nd.edu>
2023-05-03 00:14:17 -0400

    stuff

--- test6/test5/PlaySpoons.py
+++ test6/test5/PlaySpoons.py
@@ -1,15 +1,25 @@
 from SpoonsClient import *
 import sys
 
-async def main():
+def main():
     if len(sys.argv) != 2:
         print('Invalid number of arguments')
         print('Usage: python3 PlaySpoons.py <GAME_NAME>')
         exit(1)
     else:
         game_name = sys.argv[1]
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     player = SpoonsClient(game_name)
-    await player.run()
+    loop.run_until_complete(player.run())
+
+    # try:
+    #     loop.run_forever()
+    # except KeyboardInterrupt:
+    #     pass
+    # finally:
+    #     loop.close()
+    
 
 if __name__=='__main__':
-    s = asyncio.run(main())
+    main()
